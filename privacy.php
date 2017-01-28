4575f3166c24bba588a8d48a4f190fb38f97b709
Josip Bošnjak <jbosnjak@unipu.hr>
2017-01-28 19:54:00 +0100

    dodana php skripta sa pravilima o zaštiti privatnosti i podataka

---
+++ privacy.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width-device-width,initial-scale=1">
+<title>Socialnet-privacy</title>
+<link href="css/stil.css" rel="stylesheet" type="text/css" media="all">
+</head>
+
+<body>
+
+<div class="con">
+<nav>
+
+<a href="#" target="_blank">Registration</a>
+<a href="#" target="_blank">Login</a>
+
+</nav>
+</div>
+<div class="pravila">
+<section>
+<h2>Basic rules of use this site</h2>
+<ol>
+<li>When you are register and login into the site, you are agreeing to give us your sensitive data like username, email, password.</li>
+<li>We are not responsible if you give some other person your password, and if that results that that person is misusing our site , you will still be banned for our site, your serial will be blacklisted and you wont be able to register again.</li>
+<li>Serial presents email. If you make new registration from new device or email, and you still misusing our site, you will be prosecuted by law.</li>
+</ol>
+</section>
+</div>
+
+
+
+</body>
+</html>
