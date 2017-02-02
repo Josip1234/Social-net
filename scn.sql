603a37c9fb93810572d712f015f06ded589d1c30
Josip Bošnjak <jbosnjak@unipu.hr>
2017-02-02 23:47:14 +0100

    napravljena baza za registraciju i php
    napravljena baza za registraciju i php skripta za registraciju koja
    sadrži formu da se korisnik može registrirati

---
+++ scn.sql
@@ -0,0 +1,94 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 02, 2017 at 11:43 PM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 7.0.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `scn`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kvaliteta`
+--
+
+CREATE TABLE `kvaliteta` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(60) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `lastname` varchar(60) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `suggestion` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
+
+--
+-- Dumping data for table `kvaliteta`
+--
+
+INSERT INTO `kvaliteta` (`id`, `firstname`, `lastname`, `suggestion`) VALUES
+(1, 'Josip', 'Bošnjak', 'Napravljen unos feedbacka za stranicu. Popraviti bug koji dodaje prazno mjesto u tablicu. Napraviti neki captcha za odgodu novog unosa.'),
+(2, 'Josip', 'Bošnjak', 'Sada treba napraviti registracijsku formu. Treba napraviti i stranicu koja omogućuje adminu da  uređuje feedbackove. Feedbackove može vidjeti samo admin.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registration`
+--
+
+CREATE TABLE `registration` (
+  `id` int(11) NOT NULL,
+  `fname` varchar(50) COLLATE utf8_croatian_ci NOT NULL,
+  `lname` varchar(50) COLLATE utf8_croatian_ci NOT NULL,
+  `sex` varchar(1) COLLATE utf8_croatian_ci NOT NULL,
+  `dateofbirth` date NOT NULL,
+  `cityofbirth` varchar(50) COLLATE utf8_croatian_ci NOT NULL,
+  `countryofbirth` varchar(50) COLLATE utf8_croatian_ci NOT NULL,
+  `pass` varchar(50) COLLATE utf8_croatian_ci NOT NULL,
+  `profilepicture` longblob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `kvaliteta`
+--
+ALTER TABLE `kvaliteta`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `registration`
+--
+ALTER TABLE `registration`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `kvaliteta`
+--
+ALTER TABLE `kvaliteta`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `registration`
+--
+ALTER TABLE `registration`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
