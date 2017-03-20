9e1a763f0751859487580c769c86fd7985cd07eb
Josip Bošnjak <jbosnjak@unipu.hr>
2017-03-20 13:48:02 +0100

    Dodana je evidencija promijena
    Dodana je evidencija promijena samo tekstualnih stvari. Za promijenu
    slike profila, biti će nešto teže. Također, ove promijene je sada
    potrebno unijeti u bazu sa upitom za update.

--- profile.php
+++ profile.php
@@ -60,12 +60,13 @@ while($o=mysqli_fetch_array($t)){
 <label>First name:</label><br/>
 <input type="text" name="firstname1" value="<?php 
 echo $o['fname'];
+$fn=$o['fname'];
 
 ?>"/><br/><label>Last name:</label><br/>
 <input type="text" name="lastname1" value="
 <?php
 echo $o['lname'];
-
+$ln=$o['lname'];
 ?>
 
 
@@ -74,34 +75,40 @@ echo $o['lname'];
 /><br/><label>Sex:</label><br/>
 <input type="text" name="sex1" value="<?php
 echo $o['sex'];
+$s=$o['sex'];
 
 ?>"/><br/><label>Date of birth:</label><br/>
 <input type="date" name="datum1" value="<?php 
 echo $o['dateofbirth'];
+$d=$o['dateofbirth'];
 
  ?>"/><br/><label>City of birth:</label><br/>
  
  <input type="text" name="cityofbirth1" value="<?php 
 echo $o['cityofbirth'];
+$c=$o['cityofbirth'];
 
  ?>"/><br/><label>Country of birth:</label><br/>
  
  <input type="text" name="countryofbirth1" value="<?php 
 echo $o['countryofbirth'];
+$co=$o['countryofbirth'];
 
  ?>"/>
   <br/><label>Pass:</label><br/>
  <input type="text" name="pass1" value="<?php 
 echo $o['pass'];
-
+$p=$o['pass'];
  ?>"/><br/><label>Email:</label><br/>
  <input type="email" name="email1" value="<?php 
 echo $o['email'];
+$e=$o['email'];
 
 
  ?>"/><br/><label>Role:</label><br/>
  <input type="text" name="uloga1" value="<?php 
 echo $o['uloga'];
+$u=$o['uloga'];
 $ul=$_SESSION[$o['uloga']];
 if($ul=="Korisnik"){
 	
@@ -120,18 +127,56 @@ mysqli_close($dbc);
 include ("dbconn.php");
 $id1=$_SESSION['id'];
 $firstname1=$_POST['firstname1'];
-$lname1=$_POST['lname1'];
+$lastname1=$_POST['lastname1'];
 $sex1=$_POST['sex1'];
 $datum1=$_POST['datum1'];
-$cityofbirth1=$_POST['cityofbirth'];
+$cityofbirth1=$_POST['cityofbirth1'];
 $countryofbirth1=$_POST['countryofbirth1'];
 $pass1=$_POST['pass1'];
 $email1=$_POST['email1'];
-$role1=$_POST['role1'];
+$uloga1=$_POST['uloga1'];
 echo $id1;
 echo $firstname1;
+echo $lastname1;
+echo $sex1;
+echo $datum1;
+echo $cityofbirth1;
+echo $countryofbirth1;
+echo $pass1;
+echo $email1;
+echo $uloga1;
+if($_SERVER['REQUEST_METHOD']=='POST'){
+if($firstname1!=$fn){
+	echo "Promijenjeno je prvo ime";
+};
+if($lastname1!=$ln){
+	echo "Promijenjeno je prezime";
+};
+if($sex1!=$s){
+	echo "Promijenjen je spol";
+};
+if($datum1!=$d){
+	echo "Promijenjen je datum";
+}
+if($cityofbirth1!=$c){
+	echo "Promijenjen je grad";
+}
+if($countryofbirth1!=$co){
+	echo "Promijenjena je država";
+}
+if($pass1!=$p){
+	echo "Promijenjena je šifra";
+}
+if($email1!=$e){ 
+	echo "Promijenjen je email";
+}
+if($uloga1!=$u){
+	echo "Promijenjena je uloga";
+}
 mysqli_close($dbc);
-
+}else{
+	echo "";
+}
 ?>
 
 
