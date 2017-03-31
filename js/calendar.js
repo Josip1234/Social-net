88401062c6d19ebecd83e1e3b348796b418e7051
Josip Bošnjak <jbosnjak@unipu.hr>
2017-03-31 22:12:33 +0200

    napravljen kalendar za ožujak
    napravljen je kalendar za ožujak potrebno ga je malo urediti da izgleda
    ljepše na stranici. Također je i poželjno napraviti i za sve prethodne
    mjesece u 2017. godini, te preostale mjesece u godini.

--- js/calendar.js
+++ js/calendar.js
@@ -1,39 +1,45 @@
 // JavaScript Document
 
 function dohvati_kalendar(){
-	var zarezi="";
-	var novired="<br/>";
-	var dani=[];
-	var number=1;
-	var max_number=32;
-	for(;number<max_number;number+=1){
-		dani[number]=number;
-	    if(number==10){
-			
-			zarezi+=novired;
-			
-			
-		}
-		if(number==20){
-			
-			zarezi+=novired;
-			
-		}
-		if(number==30){
-			dani[number]=number;
-			zarezi+=novired;
-			
-		}
+	var razmak = "";
+    var tablica="<table>";
+	var red="<tr>";
+	var dat="<td>";
+	 var ctablica="</table>";
+	 var cred="</tr>";
+	 var cdat="</td>";
+	 
+	 
+	 razmak+=tablica;
+	 razmak+=red;
+	 for(var j=1;j<32;j++){
 		
-		zarezi+=dani[number]+"&emsp;";
+		 razmak+=dat;
+		  razmak+=j;
+		  razmak+=cdat;
+		 if(j==10){
+			 razmak+=cred;
+			 razmak+=red;
+		 }else if(j==20){
+			 razmak+=cred;
+			 razmak+=red;
+		 }else if(j==30){
+			 razmak+=cred;
+			 razmak+=red;
+		 }
+	 }
+	
+	 
+	
+	 
+	 razmak+=cred;
+	 razmak+=ctablica;
 		
-		
-		
-		
-		
-		
-	};
-    document.getElementById('calendar').innerHTML=zarezi;
+    document.getElementById('calendar').innerHTML=razmak;
+	
+	
 	
 };
+
+
 
