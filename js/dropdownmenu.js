b7fa5c7a658eaea91c80f8d7ba58159d73ffd642
Josip Bošnjak <jbosnjak@unipu.hr>
2017-05-15 15:35:30 +0200

    napravljen drop down menu
    skripta je preuzeta iz literature. Napravljen je drop down menu. Treba
    ga prilagoditi da ne bude u takvom kutu kako je kao i prilagoditi
    veličinu slova i boje pozadine prema izgledu naše sadašnje stranice.

---
+++ js/dropdownmenu.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+var ftime=100;
+var ctime=0;
+var val=0;
+function openmenu(id){
+	menucanceltime();
+	if(val) val.style.visibility='hidden';
+	val=document.getElementById(id);
+	val.style.visibility='visible';
+}
+function menuclose(){
+	if(val) val.style.visibility='hidden';
+}
+function menuclosetime(){
+	ctime=window.setftime(mclose,ftime);
+}
+function menucanceltime(){
+	if(ctime){
+		window.clearftime(ctime);
+		ctime=null;
+	}
+}
+document.onclick=menuclose;
