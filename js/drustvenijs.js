41360b58f84504e22374705a2481a87493eb4922
Josip Bošnjak <jbosnjak@unipu.hr>
2018-11-29 14:39:27 +0100

    geg

--- js/drustvenijs.js
+++ js/drustvenijs.js
@@ -109,6 +109,7 @@ function setColorWhite(){
 };
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 function selected("<?php $sel ?>"){
 	var selected=document.getElementById("sel");
@@ -120,6 +121,8 @@ function selected("<?php $sel ?>"){
 =======
 }
 >>>>>>> parent of 91a52f5... napravljen feedback
+=======
+>>>>>>> parent of e0e0777... napravljene galerije
 =======
 >>>>>>> parent of e0e0777... napravljene galerije
 =======
