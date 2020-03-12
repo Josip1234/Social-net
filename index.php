f901a852965cb883f5b23ef15aa0f847ed84e366
Josip Bošnjak <jbosnjak@unipu.hr>
2020-03-12 20:10:03 +0100

    update

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Social-net homepage</title>
+        <title>Socialnet</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/style.css" rel="stylesheet">
+        <link href="styles/style.css" rel="stylesheet" media="all">
     </head>
     <body>
-    
+    <div class="con"> 
+       <ul>
+          <li><a href="#" target="_blank">Registration</a></li>
+          <li><a href="#" target="_blank"></a>Login</li>
+       </ul>
+    </div>
     </body>
 </html>
