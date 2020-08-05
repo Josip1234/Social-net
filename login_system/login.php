a1350fdab7c19c4a26249eb5f427e4859bcb74df
Josip Bošnjak <jbosnjak@unipu.hr>
2020-08-05 21:23:40 +0200

    update

--- login_system/login.php
+++ login_system/login.php
@@ -19,8 +19,8 @@
 <div class="pravila">
 <section><h2>Login here</h2>
 <form action="login.php" method="post">
-<label>Username:</label><br/>
-<input type="text" name="username" maxlength="50" size="15" required autocomplete="off"/>
+<label>Username as email:</label><br/>
+<input type="email" name="username" maxlength="50" size="15" required autocomplete="off"/>
 <br/>
 <label>Password:</label><br/>
 <input type="password" name="pass" required size="15" autocomplete="off"/>
@@ -28,6 +28,34 @@
 <input type="submit" value="Login"/>
 
 </form>
+<?php
+include('../database/social_database_connection.php');
+
+$username=mysqli_real_escape_string($dbc,trim(strip_tags($_POST['username'])));
+if($username!=''){
+    $pass=mysqli_real_escape_string($dbc,trim(strip_tags($_POST['pass'])));
+    if($pass!=''){
+    $res="SELECT email FROM registration WHERE email='$username'";
+	mysqli_query($dbc,$res);
+	if($res){
+		$res2="SELECT id FROM registration WHERE email='$username'";
+		mysqli_query($dbc,$res2);
+		if($res2){
+            $res3="SELECT pass FROM registration WHERE email='$username'";
+			mysqli_query($dbc,$res2);
+			if($res3){
+				session_start();
+				$_SESSION['email']=$_POST['username'];
+				$_SESSION['pass']=$_POST['pass'];
+				$_SESSION['islogged']=time();
+				header('Location:../profile.php');
+			}
+		}
+	}
+}
+}
+
+?>
 </section>
 </div>
 
