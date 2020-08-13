bf2a629f4dcd215d0e422088c7739b598a0cad60
Josip Bošnjak <jbosnjak@unipu.hr>
2020-08-13 21:42:17 +0200

    added conceptual  web site elements

---
+++ home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page social network concept</title>
+</head>
+<body>
+<!-- Glavni kontejner za responsive web stranicu-->
+<div>
+</div>
+<!-- Navigacija za tražilicu, profil, albume, slika profila u obliku loga, dodavanje veze(slično kao 
+i kod facebooka, lista kontakata-->
+    <nav>
+    </nav>
+    <section>
+    <h2>Settings </h2>
+    <!--Postavke računa koje će se nalaziti na lijevoj strani stranice -->
+    <aside></aside>
+    </section>
+    <section>
+     <h2>Posts</h2>
+     <!-- Glavno mjesto za ažuriranje drugih ljudi , postovi, objave, djeljene fotografije drugih ljudi, 
+     itd itd.  -->
+    </section>
+ <!-- Login forma koja će se nalaziti na lijevoj strani -->   
+<div>
+</div>
+<!-- Registracijska forma koja će se nalaziti na desnoj strani -->
+<div></div>
+
+</body>
+</html>
