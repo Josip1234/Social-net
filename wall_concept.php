156eccf7165e00f46bd35d7f06836ddf5a760510
Josip Bošnjak <jbosnjak@unipu.hr>
2020-08-29 17:00:11 +0200

    updated wall concept

--- wall_concept.php
+++ wall_concept.php
@@ -5,6 +5,8 @@ $post_options=array("Edit post","Delete Post");
 $friend_posts_options=array("Hide post","Block post");
 $mypost="Hello it is a beautifull day";
 $my_friend_post="I cant even make it today!!";
+$date_of_addition="29.08.2020 16:55";
+$date_of_update="29.08.2020 16:56";
 
 
 
@@ -28,6 +30,7 @@ echo $user;
 <h2>Form for adding new posts from user</h2>
 <form action="wall_concept.php" method="post">
 <label for="newpost">Add new post:</label>
+<!-- for addition it will have trigger -->
 <input type="text" name="post" id="post">
 <input type="submit" value="Add new post">
 </form>
@@ -41,6 +44,8 @@ foreach ($post_options as $options) {
     echo "$options <br>";
   }
 echo $mypost;
+echo "<br> Date of addition:"+ $date_of_addition;
+echo "<br> Date of update:"+ $date_of_update;
 ?>
 <hr>
 </div>
@@ -58,6 +63,8 @@ foreach ($friend_posts_options as $options) {
 echo $friend;
 echo "<br>";
 echo $my_friend_post;
+echo "<br> Date of addition:"+ $date_of_addition;
+echo "<br> Date of update:"+ $date_of_update;
 
 ?>
 </section>
