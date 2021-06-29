5c6ab520fac5f3f95f9f2cb40cff7bd8133b1b50
Josip Bošnjak <jbosnjak@unipu.hr>
2021-06-29 12:48:02 +0200

    added registration form and database table for users

---
+++ registration.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Socialnet</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/stil.css" rel="stylesheet" type="text/css" media="all">
+    </head>
+    <body>
+    <div class="con">
+        <nav>
+            <a href="#" target="_blank">Registration</a>
+            <a href="#" target="_blank"> Login</a>
+        </nav>
+    </div>
+    <div class="pravila">
+        <section>
+            <h2>Register here</h2>
+            <form action="registration.php" method="post">
+            <label>First name:</label><br/>
+<input type="text" name="fname" autocomplete="off" maxlength="50" size="17" required/><br/>
+<label>Last name:</label><br/>
+<input type="text" name="lname" autocomplete="off" maxlength="50" size="17" required/><br/>
+<label>Sex:</label><br/>
+<input type="radio" name="spol" value="muski" required>M 
+<input type="radio" name="spol" value="zenski" required>Z 
+<br/>
+<label>Date of birth:</label><br/>
+<input type="date" name="datum_rodjenja" required/><br/>
+<label>City of birth:</label><br/>
+<input type="text" name="city_of_birth" maxlength="50" size="17" autocomplete="off"/><br/>
+<label>Country of birth:</label><br/>
+<input type="text" name="country_of_birth" maxlength="50" size="17" autocomplete="off" /><br/>
+<label>Pass:</label><br/>
+<input type="password" name="pass" maxlength="50" size="17" required autocomplete="off" /><br/>
+<label>Profile picture:</label><br/>
+<input type="file" name="slika"><br/> 
+<br/>
+<input type="submit" value="Register"/>
+            </form>
+        </section>
+    </div>
+    </body>
+</html>
--- scn.sql
+++ scn.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 26, 2021 at 04:27 PM
+-- Generation Time: Jun 29, 2021 at 12:45 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 8.0.6
 
@@ -29,8 +29,8 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `kvaliteta` (
   `id` int(11) NOT NULL,
-  `firstname` varchar(50) COLLATE utf8_croatian_ci NOT NULL,
-  `lastname` varchar(100) COLLATE utf8_croatian_ci NOT NULL,
+  `firstname` varchar(60) COLLATE utf8_croatian_ci NOT NULL,
+  `lastname` varchar(60) COLLATE utf8_croatian_ci NOT NULL,
   `suggestion` varchar(255) COLLATE utf8_croatian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
 
@@ -42,7 +42,29 @@ INSERT INTO `kvaliteta` (`id`, `firstname`, `lastname`, `suggestion`) VALUES
 (1, '', '', ''),
 (2, 'Josip', 'Bošnjak', 'Treba napraviti bla bla pa onda bla bla bla i tek onda bla bla bla.'),
 (3, 'Josip', 'Bošnjak', 'Blabla bi trebalo promijeniti staviti blablabla.'),
-(4, 'Josip', 'Bošnjak', 'Blabla bi trebalo promijeniti staviti blablabla.');
+(4, 'Josip', 'Bošnjak', 'Blabla bi trebalo promijeniti staviti blablabla.'),
+(5, 'Mario', 'Mandžukić', 'Treba mi napadačka vrba.'),
+(6, 'Ivan', 'Perišić', 'Imam coronu. Ne mogu igrati.'),
+(7, 'Josip', 'Bošnjak', 'Napravljen unos feedbacka za stranicu. Popraviti bug koji dodaje prazno mjesto u tablicu. Napraviti neki captcha za odgodu novog unosa.'),
+(8, 'Josip', 'Bošnjak', 'Sada treba napraviti registracijsku formu. Treba napraviti i stranicu koja omogućuje adminu da  uređuje feedbackove. Feedbackove može vidjeti samo admin.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registration`
+--
+
+CREATE TABLE `registration` (
+  `id` int(11) NOT NULL,
+  `fname` varchar(50) COLLATE utf8_croatian_ci NOT NULL,
+  `lname` varchar(50) COLLATE utf8_croatian_ci NOT NULL,
+  `sex` varchar(1) COLLATE utf8_croatian_ci NOT NULL,
+  `dateofbirth` date NOT NULL,
+  `cityofbirth` varchar(50) COLLATE utf8_croatian_ci NOT NULL,
+  `countryofbirth` varchar(50) COLLATE utf8_croatian_ci NOT NULL,
+  `pass` varchar(50) COLLATE utf8_croatian_ci NOT NULL,
+  `profilepicture` longblob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_croatian_ci;
 
 --
 -- Indexes for dumped tables
@@ -58,6 +80,14 @@ ALTER TABLE `kvaliteta`
   ADD KEY `suggestion` (`suggestion`);
 
 --
+-- Indexes for table `registration`
+--
+ALTER TABLE `registration`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fname` (`fname`),
+  ADD KEY `lname` (`lname`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -65,7 +95,13 @@ ALTER TABLE `kvaliteta`
 -- AUTO_INCREMENT for table `kvaliteta`
 --
 ALTER TABLE `kvaliteta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `registration`
+--
+ALTER TABLE `registration`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
