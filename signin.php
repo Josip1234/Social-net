9633247ed27265487f6a6c8b451c5be8db4d9f2c
Josip Bošnjak <jbosnjak3@gmail.com>
2022-02-17 20:49:43 +0100

    added form for login

---
+++ signin.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Social-net sign in</title>
+</head>
+
+<body>
+	
+	<form action="signin.php" method="post">
+	<input type="text" name="username"  placeholder="Enter username or email">
+	<input type="password" name="password" placeholder="Enter your password">
+	<input type="submit" value="Sign in">
+	</form>
+	
+	
+</body>
+</html>
