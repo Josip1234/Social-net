35d00a44e98c5c1ea57f9fafb0f2a279ecdbfd03
Josip Bošnjak <jbosnjak3@gmail.com>
2022-09-17 14:07:43 +0200

    Update users.php
    added attributtes

--- php_scripts/php_classes/users.php
+++ php_scripts/php_classes/users.php
@@ -1,3 +1,12 @@
 <?php 
-
+class User{
+public $first_name;
+public $last_name;
+public $email;
+public $sex;
+public $profile;
+private $date_of_birth;
+private $address;
+private $password;
+}
 ?>
