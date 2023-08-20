e7e1fb5d2c9d5c06334922a7eab9340d817aea1e
Josip Bošnjak <jbosnjak3@gmail.com>
2023-08-20 19:40:22 +0200

    update

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Socialnet</title>
+    <link rel="stylesheet" href="css/stil.css">
+</head>
+<body>
+    <div class="con">
+        <nav>
+            <ul>
+                <li><a href="#" target="_blank" rel="noopener noreferrer">Registration</a></li>
+                <li><a href="#" target="_blank" rel="noopener noreferrer">Login</a></li>
+            </ul>
+        </nav>
+
+        <div class="rules">
+            <section>
+                <h2>Privacy rules</h2>
+                <p>
+                    We guarantee the protection of your data. If there is any violation of data integrity or if your data is stolen, we are responsible for it if it happens on the server. If the user is at fault, we disclaim responsibility and shift the responsibility to the user. For more detailed information, visit this link:
+                    <a id="privacy" href="#" target="_blank" rel="noopener noreferrer">Privacy</a>
+                </p>
+            </section>
+
+        </div>
+
+    </div>
+
+</body>
+</html>
