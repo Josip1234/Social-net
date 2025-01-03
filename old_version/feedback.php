45c021e40b733bb01e888933f3805b6d39ed26ba
Josip Bošnjak <jbosnjak3@gmail.com>
2025-01-03 22:25:52 +0100

    update

---
+++ old_version/feedback.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+<head>
+<?php
+include("dbconn.php");
+?>
+<meta charset="utf-8">
+<meta name="viewport" content="width-device-width,initial-scale=1">
+<title>Socialnet</title>
+<link href="css/stil.css" rel="stylesheet" type="text/css" media="all">
+</head>
+
+<body>
+
+<div class="con">
+<nav>
+
+<a href="#" target="_blank">Registation</a>
+<a href="#" target="_blank">Login</a>
+
+</nav>
+</div>
+<div class="pravila">
+<section>
+<h2>Your feedback</h2>
+<form action="feedback.php" method="post">
+<label>Your first name:</label><br/>
+<input type="text" name="fname"/><br/>
+<label>Your last name:</label><br/>
+<input type="text" name="lname"/><br/>
+<label>Suggestions of improvment:</label><br/>
+<textarea name="suggestions"></textarea>
+<br/>
+<input type="submit" value="Send suggestion"/>
+</form>
+</section>
+</div>
+
+
+
+</body>
+</html>
--- old_version/privacy.php
+++ old_version/privacy.php
@@ -24,6 +24,12 @@
 <li>When you are register and login into the site, you are agreeing to give us your sensitive data like username, email, password.</li>
 <li>We are not responsible if you give some other person your password, and if that results that that person is misusing our site , you will still be banned for our site, your serial will be blacklisted and you wont be able to register again.</li>
 <li>Serial presents email. If you make new registration from new device or email, and you still misusing our site, you will be prosecuted by law.</li>
+<li>Any criminal activity, drug dealing, or some other things will get you punished. </li>
+<li>Dont insult others and behave yourself good on this site. If you break the rule, you will get some negative points. 
+If you collect more than 50 negative points, you will be banned for 1 day. </li>
+<li>For every other 50 points, you will be banned for 2 days, other 4 days, etc.</li>
+<li>Your data will have a daily backup. If you want some backuped files to have on your computer, just give us some feedback. Use <a href="feedback.php" target="_blank">feedback</a> form, to get us request, you will get your data through email.</li>
+<li>You can in your feedback also, sent us some suggestions to improve our page like design, to add some additional content, etc..</li>
 </ol>
 </section>
 </div>
