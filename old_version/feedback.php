f40a7ec69b337ed112cf4362d43b5503fe583d02
Josip Bošnjak <jbosnjak3@gmail.com>
2025-01-04 14:14:37 +0100

    update

--- old_version/feedback.php
+++ old_version/feedback.php
@@ -25,14 +25,39 @@ include("dbconn.php");
 <h2>Your feedback</h2>
 <form action="feedback.php" method="post">
 <label>Your first name:</label><br/>
-<input type="text" name="fname"/><br/>
+<input type="text" name="fname" required autocomplete="off"/><br/>
 <label>Your last name:</label><br/>
-<input type="text" name="lname"/><br/>
+<input type="text" name="lname" required autocomplete="off"/><br/>
 <label>Suggestions of improvment:</label><br/>
-<textarea name="suggestions"></textarea>
+<textarea name="suggestions" required></textarea>
 <br/>
 <input type="submit" value="Send suggestion"/>
 </form>
+<?php
+$fname=mysqli_real_escape_string($dbc,trim(strip_tags($_POST['fname'])));
+//riješen problem dodavanja praznih redaka za fname, lname i suggestions
+if($fname!=''){
+	//strip_tags uklanjaju php i html oznake, trim uklanja nepotreban razmak ili druge znakove sa oba kraja stringa 
+	//mysqli_real_escape sa ostalim objašnjenim funkcijama sprječavaju sql injekciju
+$lname=mysqli_real_escape_string($dbc,trim(strip_tags($_POST['lname'])));
+if($lname!=''){
+$suggestions=mysqli_real_escape_string($dbc,trim(strip_tags($_POST['suggestions'])));
+if($suggestions!=''){
+$query="INSERT INTO kvaliteta (firstname,lastname,suggestion) VALUES ('$fname','$lname','$suggestions')";
+mysqli_query($dbc,$query);
+mysqli_close($dbc);
+include("functions.php");
+if($query){
+	die("Thanks for adding some suggestions");
+}else{
+	die("Error! Information not inserted!");
+}
+}
+}
+}else{
+	die("Can 't add empty informations");
+}
+?>
 </section>
 </div>
 
