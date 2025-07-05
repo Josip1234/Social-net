be47c8762770f26a9f462cb59029b9806038a86c
Josip Bošnjak <jbosnjak3@gmail.com>
2025-07-05 14:34:16 +0200

    update
    clendar has been made

--- classes/body_html.php
+++ classes/body_html.php
@@ -2,7 +2,7 @@
 
 class Body{
 const OPEN_BODY="<body>";
-const OPEN_BODY_CUSTOMISED="<body class='rounded-5 orange outset m-3 p-1'>";
+const OPEN_BODY_CUSTOMISED="<body class='rounded-5 orange outset m-3 p-1'";
 const CLOSE_BODY="</body>";
 const OPEN_CONTAINER="<div class='container-fluid text-center'>";
 const CLOSE_DIV="</div>";
@@ -26,6 +26,10 @@ const CLOSE_HTML_DOCUMENT="</html>";
 const OPEN_HORIZONTAL_UL="<ul class='list-group list-group-horizontal'>";
 const OPEN_LIST_GROUP_ITEM="<li class='list-group-item red border border-0 rounded-pill'>";
 const OPEN_NAV_CUSTOMISED="<nav class='red border border-0 rounded-pill p-2'>";
+const OPEN_SECTION_CALENDAR="<section id='calendar' class='calendar'>";
+const OPEN_DIV_CALENDAR="<div id='calendar' class='calendar'>";
+const OPEN_BODY_ONMOUSEOVER_CALENDAR_ONLY="<body class='rounded-5 orange outset m-3 p-1' onmouseover='dohvati_kalendar_nova_verzija()'>";
+const OPEN_BODY_ONLOAD_CALENDAR_ONLY="<body class='rounded-5 orange outset m-3 p-1' onload='dohvati_kalendar_nova_verzija()'>";
 }
 
 ?>
--- js_scripts/calendar.js
+++ js_scripts/calendar.js
@@ -21,13 +21,13 @@ function dohvati_kalendar(){
 }
 //kalendar u obliku tablice za treći mjesec
 function dohvati_kalendar_nova_verzija(){
-   var razmak="";
-   var tablica="<table>";
+   var razmak=" ";
+   var tablica="<h2>Calendar For March 2025</h2><table>";
    var red="<tr>";
    var dat="<td>";
    var ctablica="</tablica>";
-   var cred="</tr>";
-   var cdat="</td>";
+   var cred="</tr> ";
+   var cdat="</td> ";
    razmak+=tablica;
    razmak+=red;
    for(var j=1;j<32;j++){
@@ -48,8 +48,7 @@ function dohvati_kalendar_nova_verzija(){
    razmak+=cred;
    razmak+=ctablica;
 document.getElementById('calendar').innerHTML=razmak;
-document.getElementById('calendar').style.backgroundColor='black';
-document.getElementById('calendar').style.width='120%';
+document.getElementById('calendar').style.width='50%';
 document.getElementById('calendar').style.height='100%';
 document.getElementById('calendar').style.overflow='auto';
 }
