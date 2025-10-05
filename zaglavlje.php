ab4a8624c5d1ff801e8c4e74ad68e14a6300ce1e
Josip Bošnjak <jbosnjak3@gmail.com>
2025-10-05 14:03:45 +0200

    update
    added css for styling our website added css in zaglavlje

--- zaglavlje.php
+++ zaglavlje.php
@@ -8,6 +8,7 @@ include "scripts/functions.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Drustvena mreza</title>
+    <link rel="stylesheet" href="styles/style.css">
 </head>
 <body>
 <header>
