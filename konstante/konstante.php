f0628951a840c6d6fa10b09fa4f186f1e552be23
Josip Bošnjak <jbosnjak3@gmail.com>
2025-10-06 22:26:12 +0200

    update
    added some constants added log.json will be used as a log file when and where user has done something, content will not be written, only time so we can now when something went wrong, when this has happend

---
+++ konstante/konstante.php
@@ -0,0 +1,11 @@
+<?php 
+//ovdje će biti sve konstante vrijednosti koje ćemo pisati u ovom projektu
+define("PRIJAVA","Korisnik se prijavio:");
+define("REGISTRACIJA","Korisnik se registrirao:");
+define("UPIS","Korisnik je upisao:");
+define("BRISANJE","Korisnik je obrisao:");
+define("AZURIRANJE","Korisnik je azurirao:");
+
+
+
+?>
