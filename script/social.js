5168c54fa5fd6d83fb95e254864f533e263a2f8f
Josip Bošnjak <jbosnjak3@gmail.com>
2025-11-16 09:42:27 +0100

    new social network application will be started from scratch

--- script/social.js
+++
@@ -1,9 +0,0 @@
-//funkcija koja će prikazati trenutno vrijeme i brojati će se dinamički
-setInterval(ispisiVrijeme,0);
-function ispisiVrijeme(){
-  
-   const d = new Date();
-let text = d.toLocaleTimeString();
-document.getElementById("trenutno_vrijeme").innerHTML = text;
-
-}
