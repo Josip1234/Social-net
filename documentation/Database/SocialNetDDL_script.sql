095fa274388c72b6bd63c9a5879e11c72bfba0cc
Josip Bošnjak <jbosnjak3@gmail.com>
2025-11-27 21:30:35 +0100

    update
    updated table state added table city with data

--- documentation/Database/SocialNetDDL_script.sql
+++ documentation/Database/SocialNetDDL_script.sql
@@ -1,3 +1,8 @@
 create database SocialNet character set utf8mb4 collate utf8mb4_unicode_ci;
 use socialnet;
-create table state (stateId int auto_increment primary key, name varchar(100) not null,unique(name));
+create table state (stateId int unsigned auto_increment primary key, name varchar(100) not null,unique(name));
+create table city (
+postNumber varchar(20) primary key, 
+name varchar(150) not null,
+stateId int unsigned not null, 
+constraint stateId_fk foreign key (stateId) references state (stateId) on delete cascade on update cascade);
--- documentation/Database/SocialNetDML_script.sql
+++ documentation/Database/SocialNetDML_script.sql
@@ -1,1 +1,5 @@
-INSERT INTO state(name) values ('Hrvatska'),('Italija'),('Francuska');
+INSERT INTO state(name) values ('Croatia'),('Italy'),('France'),('USA'),('Mexico'),('Uruguay'),('Brazil'),('Ukraine'),('Saudi Arabia');
+INSERT INTO state(name) values ('Serbia'),('Montenegro'),('Bosnia and Herzegovina'),('Hungary'),('China'),('Taiwan'),('Bangladesh');
+INSERT INTO state(name) values ('Australia'),('New Zeland'),('Denmark'),('United Kingdom'),('Wales'),('Germany'),('Spain');
+
+insert into city values ('34000','Požega','1'),('10000','Zagreb','1'),('35000','Slavonski Brod',1);
