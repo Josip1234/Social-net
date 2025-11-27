c65d6f0d2fe00779441b2ece3957efd2c87cd1ce
Josip Bošnjak <jbosnjak3@gmail.com>
2025-11-27 21:37:20 +0100

    update
    added first version database dump

--- documentation/Database/SocialNetDML_script.sql
+++ documentation/Database/SocialNetDML_script.sql
@@ -3,3 +3,4 @@ INSERT INTO state(name) values ('Serbia'),('Montenegro'),('Bosnia and Herzegovin
 INSERT INTO state(name) values ('Australia'),('New Zeland'),('Denmark'),('United Kingdom'),('Wales'),('Germany'),('Spain');
 
 insert into city values ('34000','Požega','1'),('10000','Zagreb','1'),('35000','Slavonski Brod',1);
+insert into city values ('10450','Jastrebarsko','1'),('44330','Novska','1'),('47240','Slunj',1);
