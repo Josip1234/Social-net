97cfe7cddb8316c8787713ba228359a470e9c19f
Josip Bošnjak <jbosnjak3@gmail.com>
2025-12-14 21:03:12 +0100

    update
    finished model data for profile topics subtopics comments

--- documentation/Database/SocialNetDDL_script.sql
+++ documentation/Database/SocialNetDDL_script.sql
@@ -1,16 +1,20 @@
 create database SocialNet character set utf8mb4 collate utf8mb4_unicode_ci;
 use socialnet;
+
 create table state (stateId int unsigned auto_increment primary key, name varchar(100) not null,unique(name));
+
 create table city (
 postNumber varchar(20) primary key, 
 name varchar(150) not null,
 stateId int unsigned not null, 
 constraint stateId_fk foreign key (stateId) references state (stateId) on delete cascade on update cascade);
+
 create table address (
 addressId int unsigned primary key not null auto_increment,
 street varchar(255) not null,
 postNumber varchar(20) not null,
 constraint postNumber_fk foreign key (postNumber) references city (postNumber) on delete cascade on update cascade);
+
 create table profile(
 userId int unsigned primary key not null auto_increment,
 firstName varchar(50) not null,
@@ -22,12 +26,14 @@ addressId int unsigned null,
 hashPassword text not null,
 unique(email),
 constraint addressId_fk foreign key (addressId) references address (addressId) on delete cascade on update cascade);
+
 create table accounttype(
 acTypeId int unsigned auto_increment primary key not null,
 acTypeName varchar(30) not null,
 listOfPrivileges text not null,
 unique(acTypeName),
 unique(listOfPrivileges));
+
 create table profiledetails(
 proDetId int unsigned primary key auto_increment not null,
 userId int unsigned not null,
@@ -37,6 +43,7 @@ pdUpdateDate datetime not null,
 accountStatus enum('Active','Banned','Inactive'),
 constraint userIdac_fk foreign key (userId) references profile (userId) on update cascade on delete cascade,
 constraint acTypeId_fk foreign key (acTypeId) references accounttype (acTypeId) on delete SET NULL on update cascade); 
+
 -- if profile is updated or deleted, update or delete also profile details
 -- if type is deleted, set null, if it is updated update also here 
 create table imageType(
@@ -44,6 +51,7 @@ typeId int unsigned primary key auto_increment not null,
 iTypeName enum('.jpg','.jpeg','.png','.gif','.webp','.svg'),
 unique(iTypeName));
 -- one image can have one multiple details
+
 create table image(
 imageId int unsigned primary key auto_increment not null,
 userId int unsigned not null,
@@ -85,3 +93,46 @@ dateDeleted datetime null,
 dateUpdated datetime null,
 dateAdded datetime not null,
 constraint dbLogId_fk foreign key(dbLogId) references database_logger(dbLogId) on update cascade on delete cascade);
+
+create table topics(
+topicId int unsigned primary key auto_increment,
+userId int unsigned not null,
+topicContent text not null,
+topicDateAdded datetime not null,
+topicLike integer unsigned not null,
+topicDislike integer unsigned not null,
+topicDateUpdated datetime not null,
+unique(topicContent),
+constraint user_id_topics_fk foreign key(userId) references profile(userId) on update cascade on delete cascade);
+
+create table subtopics(
+subTopicsId int unsigned not null primary key auto_increment,
+topicId int unsigned not null,
+subTopicContent text not null,
+subTopicDateAdded datetime not null,
+subTopicDateUpdated datetime not null,
+subtopicLike integer unsigned not null,
+subtopicDislike integer unsigned not null,
+unique(subTopicContent),
+constraint topicId_fk foreign key (topicId) references topics(topicId) on update cascade on delete cascade);
+
+create table comments(
+commentId int unsigned not null primary key auto_increment,
+commentContent text not null,
+markOfOffensiveness enum('Offensive','Not offensive'),
+comDateUpdated datetime not null,
+comDateAdded datetime not null,
+commentLike integer unsigned not null,
+commentDislike integer unsigned not null);
+
+create table procomsub(
+proComSub int unsigned not null primary key auto_increment,
+userId int unsigned not null,
+commentId int unsigned not null,
+subTopicsId int unsigned not null,
+constraint userIdPcs_fk foreign key (userId) references profile(userId) on update cascade on delete cascade,
+constraint commentId_fk foreign key(commentId) references comments(commentId) on update cascade on delete cascade,
+constraint subtopicsid_pcs_fk foreign key (subTopicsId) references subtopics(subTopicsId) on update cascade on delete cascade);
+
+
+
