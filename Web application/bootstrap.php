0c2b36a2a2f3bf8ae7b29efb02b66cb763908321
Josip1234 <jbosnjak3@gmail.com>
2026-01-22 09:46:48 +0100

    Update bootstrap.php
    added loading environment depending of user if user is social admin env should have error reporting included on website otherwise turn it off for regular users

---
+++ Web application/bootstrap.php
@@ -0,0 +1,27 @@
+<?php 
+use Dotenv\Dotenv;
+//session start
+session_start();
+//load composer autoload
+require_once __DIR__.'/vendor/autoload.php';
+//environment before loading env files
+$appEnv=$_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'social_admin';
+//env file choosing
+$envFile=match ($appEnv) {
+     'social_admin'=> '.env.social_admin',
+     'regular'=> '.env.regular',
+     default => '.env.social_admin'
+};
+//load .env
+$dotenv=Dotenv::createImmutable(__DIR__,$envFile);
+$dotenv->load();
+//safe reading APP_ENV after load
+$currentEnv=$_ENV['APP_ENV']??$appEnv;
+//error handling
+if($currentEnv==='social_admin'){
+    ini_set('display_errors',1);
+    error_reporting(E_ALL);
+}else{
+    ini_set('display_errors',0);
+}
+define('APP_ENVIRONMENT',$currentEnv);
