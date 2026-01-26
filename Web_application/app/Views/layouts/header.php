3f01c1956a478a4a0b9b2f121e457158c382fb62
Josip1234 <jbosnjak3@gmail.com>
2026-01-26 17:12:09 +0100

    update
    added homepage layout pages added content for header added footer to main page included

---
+++ Web_application/app/Views/layouts/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Social-net</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <header>
+        <div class="nav">
+<nav>
+<ul>
+<li><a href="#" target="_blank">Registration form</a></li>
+<li><a href="#" target="_blank">Login form</a></li>
+</ul>
+</nav>
+</div>
+    </header>
