b0c88cd1481949b7920cd722b4f4e8360e050614
Josip1234 <jbosnjak3@gmail.com>
2026-03-05 17:51:57 +0100

    Update City.php
    added comments

--- Web_application/app/Models/City.php
+++ Web_application/app/Models/City.php
@@ -1,5 +1,10 @@
 <?php 
 namespace App\Models;
 class City{
-    
+    //function for searching city name 
+    //need postal number first 
+    //if postal number already exists, 
+    //return count 1 
+    //otherwise return count 2
+   // SELECT count(c.postNumber) as data FROM city c where c.name='Zagreb';
 }
