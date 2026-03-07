fb61acae2550f0bdeb841adbf03e010b370d760f
Josip1234 <jbosnjak3@gmail.com>
2026-03-07 17:09:33 +0100

    update

--- Web_application/app/Views/home/register.php
+++ Web_application/app/Views/home/register.php
@@ -28,9 +28,13 @@
           <?php else: ?>
         <h2>User registration</h2>
         <form method="post" action="<?=  htmlspecialchars($_SERVER["PHP_SELF"].'?page=register');?>">
+                   
+
+         
+
             <?php if(isset($_POST)): ?>
             <label for="fname">First name:</label>
-            <input type="text" name="fname" id="fname" value="<?= isset($_POST["fname"])??$_POST["fname"]; ?>">
+            <input type="text" name="fname" id="fname" value="<?= isset($_POST["fname"])?$_POST["fname"]:"" ?>">
             <?php if(isset($errors["fn"])): ?>
             <span class="error"> <?= $errors["fn"]; ?></span>
             <?php endif; ?>
@@ -38,7 +42,7 @@
             <span class="error"> <?=  $errors["fnn"]; ?></span>
             <?php endif; ?>
             <label for="lname">Last name:</label>
-            <input type="text" name="lname" id="lname" value="<?= isset($_POST["lname"])??$_POST["lname"] ?>">
+            <input type="text" name="lname" id="lname" value="<?= isset($_POST["lname"])?$_POST["lname"]:"" ?>">
             
                <?php if(isset($errors["ln"])): ?>
             <span class="error"> <?= $errors["ln"]; ?></span>
@@ -51,7 +55,7 @@
 
 
             <label for="email">Email address:</label>
-            <input type="email" name="email" id="email" value="<?= isset($_POST["email"])??$_POST["email"]; ?>">
+            <input type="email" name="email" id="email" value="<?= isset($_POST["email"])?$_POST["email"]:"" ?>">
             
                  <?php if(isset($errors["em"])): ?>
             <span class="error"> <?= $errors["em"]; ?></span>
@@ -83,7 +87,7 @@
             </span>
      
             <label for="dbirth">Date of birth</label>
-            <input type="date" name="dbirth" id="dbirth" value="<?= isset($_POST["dbirth"])??$_POST["dbirth"]; ?>" max="999-12-31">
+            <input type="date" name="dbirth" id="dbirth" value="<?= isset($_POST["dbirth"])?$_POST["dbirth"]:"" ?>" max="999-12-31">
                       <?php if(isset($errors["db"])): ?>
             <span class="error"> <?= $errors["db"]; ?></span>
             <?php endif; ?>
@@ -109,11 +113,19 @@
                     <label for="state">Select state</label>
                    <select name="state" id="state" onchange="showSelected()">
                       <option value="0">--- Select state ---</option>
-                      <?php 
-                      foreach ($states as $state):
+               
+                       <?php    
+                  
+                    foreach ($states as $state):
                       ?>
-                      <option value="<?= $state["stateId"] ?>"><?= $state["name"] ?></option>
-                      <?php endforeach; ?>
+                      
+                     <option value="<?=$state["stateId"]; ?>"
+                     <?= $_POST["state"]===$state["stateId"]?:"selected"; ?>> <?= $state["name"]; ?></option>
+                   
+                      
+                
+                
+                <?php endforeach; ?>
                    </select>
                  <span class="error">Errors for state</span>
                  <?php
@@ -121,7 +133,7 @@
                  
                  $selected=isset($_COOKIE["selected"])??0;
              
-                  echo $selected;
+                
                 
                   
                  ?>
