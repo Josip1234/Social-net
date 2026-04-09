f118b09264f6d2552207fd850f8ba48f00889599
Josip1234 <jbosnjak3@gmail.com>
2026-04-09 13:47:26 +0200

    update
    updated query for selection profile data added selection of user id  added in profile details separated url links to update informations about user profile details images and addresses

--- Web_application/app/Views/users/profile.php
+++ Web_application/app/Views/users/profile.php
@@ -16,12 +16,23 @@
 
                 <label for="dbirth">Date of birth</label>
             <input type="date" name="dbirth" id="dbirth" value="<?= $profil["dateOfBirth"]; ?>" max="999-12-31" readonly>
-
+            <span id="updateBasicInfo">
+                 <?php echo "<a href='?id={$profil["userId"]}'>Update basic information</a>"; ?>
+            </span>
             <label for="img">Profile image:</label>
             <img src="<?= $profil["url"]; ?>" alt="<?= $profil["imageName"] ?>" id="img" readonly>
 
+            <span id="updateProfileImage">
+                 <?php echo "<a href='?id={$profil["userId"]}'>Update profile image</a>"; ?>
+            </span>
+
              <label for="acTypeId">Account type:</label>
             <input type="text" name="acTypeId" id="acTypeId" value="<?= $profil["acTypeName"]; ?>" readonly>
+
+            <!-- this must be visible only for admins -->
+             <span id="updateAccountType">
+                 <?php echo "<a href='?id={$profil["userId"]}'>Update account type</a>"; ?>
+            </span>
 
             <label for="accountStatus">Account status:</label>
             <input type="text" name="accountStatus" id="accountStatus" value="<?= $profil["accountStatus"]; ?>" readonly>
@@ -29,8 +40,14 @@
             <label for="registrationDate">Registration date</label>
             <input type="datetime" name="registrationDate" id="registrationDate" value="<?= $profil["registrationDate"]; ?>" readonly>
 
+                 <span id="updateProfileDetails">
+                 <?php echo "<a href='?id={$profil["userId"]}'>Update profile details</a>"; ?>
+            </span>
+
+
                <label for="street">Street</label>
             <input type="text" name="street" id="street" value="<?= $profil["street"]; ?>" readonly>
+            
 
               <label for="city">City</label>
             <input type="text" name="city" id="city" value="<?= $profil["CitName"]; ?>" readonly>
@@ -38,6 +55,9 @@
                  <label for="state">State</label>
             <input type="text" name="state" id="state" value="<?= $profil["StName"]; ?>" readonly>
 
+                   <span id="updateAddress">
+                 <?php echo "<a href='?id={$profil["userId"]}'>Update address</a>"; ?>
+            </span>
          </form>
      
     </div>
