52933c23dba3ea0bafeea1db296ae9579e39c2ca
Josip1234 <jbosnjak3@gmail.com>
2026-04-14 18:02:57 +0200

    update
    dodan insert ili update korisničke uloge ovisno o tome da li korisnik ima već dodjeljenu ulogu ili ne

--- OldSocialNetV1/dodjeli_uloge.php
+++ OldSocialNetV1/dodjeli_uloge.php
@@ -37,12 +37,58 @@ while($row=mysqli_fetch_array($q)){
 	echo "</tr>";
 };
 echo "</tbody></table>";
-mysqli_close($dbc);
+
 ?>
 </section>
 </div>
 
+<div id="dodjela_uloga">
+	<section>
+<h2>Set the role for one user:</h2>
+<form action="dodjeli_uloge.php" method="post">
+<label>Email of the user which you want so set his role:</label><input type="email" autocomplete="off" name="email">
+<select name="selektiraj">
+<option value="Administrator">Administrator</option>
+<option value="Korisnik">Korisnik</option>
+<option value="Banovani korisnik">Banovani korisnik</option>
+</select>
+<input type="submit" value="Set_role"/>
+</form>
+</section>
+<?php 
+if($_SERVER["REQUEST_METHOD"]==="POST"){
+		$email=$_POST["email"];
+		$userId="SELECT r.id FROM registration r where email='$email'";
+		$query=mysqli_query($dbc,$userId);
+		$result=mysqli_fetch_assoc($query);
+		$uId=$result["id"];
+		$selektiraj=$_POST["selektiraj"];
+		//check if there is a user with already assigned user role if there is update his role 
+		//else insert new role
+		$query="SELECT count(u.user_id) as UserRoleNum from uloge u where user_id='$uId'";
+		$stmt=mysqli_query($dbc,$query);
+		$res=mysqli_fetch_assoc($stmt);
+		if($res["UserRoleNum"]>0){
+			$sql="UPDATE uloge set uloga='$selektiraj' where user_id='$uId'";
+			$stmt=mysqli_query($dbc,$sql);
+			if($stmt){
+				echo "Successfully updated user role.";
+			}
+		}else{
+			$sql="INSERT INTO uloge (user_id,uloga) VALUES ('$uId','$selektiraj')";
+			$stmt=mysqli_query($dbc,$sql);
+			if($stmt){
+				echo "Successfully added new user role.";
+			}
+		}
 
+}
+
+
+mysqli_close($dbc); 
+
+?>
+</div>
 
 </body>
 </html>
