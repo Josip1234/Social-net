232d7180672e31fcdbc62076b115aea1a61e2ad6
Josip1234 <jbosnjak3@gmail.com>
2026-05-19 14:29:18 +0200

    update
    added more random images in randomslike.js

--- OldSocialNetV1/randomslike.js
+++ OldSocialNetV1/randomslike.js
@@ -1,5 +1,7 @@
 function slike(){
-	var slik=["images/chuckolino.jpg"];
+	var slik=["images/chuckolino.jpg","images/6909249-black-hd-background.jpg","images/mountain_tree_nature_winter_snow_sky_snowy_mountains-7836.jpg",
+		"images/WIN_20170103_10_27_47_Pro.jpg"
+	];
 
 var b=Math.floor(Math.random()*slik.length);
 document.getElementById('s').innerHTML="<img src='"+slik[b]+"' alt='"+b+"'>";
