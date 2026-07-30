9913d591b6d34304ebba530bac22a92437d16127
Josip1234 <jbosnjak3@gmail.com>
2026-07-30 09:38:30 +0200

    update

---
+++ OldSocialNetV4/db/social_database_connection.php
@@ -0,0 +1,14 @@
+<?php
+namespace Db;
+$host="localhost";
+$user="adminscn";
+$pass="admin";
+$db="scn3";
+
+$dbc=mysqli_connect($host,$user,$pass,$db);
+mysqli_set_charset($dbc,'utf8');
+
+ini_set('display_errors',0);
+if(!$dbc){
+    die("I cant connect or no internet connection");
+}
---
+++ OldSocialNetV4/functions/functions.php
@@ -0,0 +1,2 @@
+<?php 
+namespace Functions;
